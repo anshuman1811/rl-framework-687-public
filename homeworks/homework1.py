b61157e5d74295f9e99b691f177ebcd8c075f277
Anshuman Mishra <anshuman@1x-nat-vl931-172-30-151-199.wireless.umass.edu>
2019-09-25 14:40:56 -0400

    Homework 1

--- homeworks/homework1.py
+++ homeworks/homework1.py
@@ -1,5 +1,7 @@
 import numpy as np
 from rl687.environments.gridworld import Gridworld
+import matplotlib.pyplot as plt
+import time
 
 def problemA():
     """
@@ -7,7 +9,40 @@ def problemA():
     Report the mean, standard deviation, maximum, and minimum of the observed 
     discounted returns.
     """
-    pass  # TODO
+    # setting random seed for reproducibility
+    print ("Problem A")
+
+    env = Gridworld()
+    
+    discounted_returns = []
+    for episode in range(10000):
+        # print (episode)
+        discounted_return = 0.0
+        while not env.isEnd:
+            state = env.state
+            action = np.random.choice([0,1,2,3])
+            # print (state, action)
+            actual_action, new_state, reward = env.step(action)
+            # print (actual_action, new_state, reward)
+            discounted_return += reward
+            # print (t)
+        env.reset()
+        # print(time_step)
+        discounted_returns.append(discounted_return)
+
+    print ("Mean ", np.mean(discounted_returns))
+    print ("Std Dev ", np.std(discounted_returns))
+    print ("Max ", np.max(discounted_returns))
+    print ("Min ", np.min(discounted_returns))
+
+    return discounted_returns
+    """
+    Results:
+        Mean  -9.123800743342608
+        Std Dev  7.197419765658342
+        Max  4.304672100000001
+        Min  -46.72965105421902
+    """
 
 def problemB():
     """
@@ -15,10 +50,113 @@ def problemB():
     mean, standard deviation, maximum, and minimum of the observed 
     discounted returns
     """
-    pass  # TODO
+    print ("Problem B")
+
+    optimal_policy_actions = [1,1,1,1,2,  0,1,1,1,2,  0,2,-1,2,2,  0,3,-1,1,2,  0,3,1,1,-1]
+
+    env = Gridworld()
     
+    discounted_returns = []
+    for t in range(10000):
+        # print (t)
+        discounted_return = 0.0
+        while not env.isEnd:
+            state = env.state
+            action = optimal_policy_actions[state]
+            # print (state, action)
+            actual_action, new_state, reward = env.step(action)
+            # print (actual_action, new_state, reward)
+            discounted_return += reward
+        discounted_returns.append(discounted_return)
+        env.reset()
+
+    print ("Mean ", np.mean(discounted_returns))
+    print ("Std Dev ", np.std(discounted_returns))
+    print ("Max ", np.max(discounted_returns))
+    print ("Min ", np.min(discounted_returns))
+
+    return discounted_returns
+    # plt.hist(sorted(discounted_returns), density = True, cumulative=True, label='CDF',
+    #      histtype='step', alpha=0.8, color='k')
+    # plt.show()
+
+    """
+    Results:
+        Mean  2.670185828345252
+        Std Dev  3.2895320487842836
+        Max  4.782969000000001
+        Min  -25.05248903619
+    """
+
+def problemE():
+    """
+    Have the agent uniformly randomly select actions. Run 10,000 episodes.
+    Report the mean, standard deviation, maximum, and minimum of the observed 
+    discounted returns.
+    """
+    # setting random seed for reproducibility
+    print ("Problem E")
+    start_time = time.time()
+
+    env = Gridworld(startState = 19)
+    num_episodes = 1000000
+    count_s19_22_given_s8_19 = 0
+    for episode in range(num_episodes):
+        # print (episode)
+        time_step = 0
+        while (not env.isEnd) and time_step<12:
+            state = env.state
+            if time_step == 11 and state == 22:
+                count_s19_22_given_s8_19 += 1
+            action = np.random.choice([0,1,2,3])
+            env.step(action)
+            time_step += 1
+            # print (t)
+        env.reset()
+    print(count_s19_22_given_s8_19)
+    Pr_s19_22_given_s8_19 = (count_s19_22_given_s8_19*1.0)/num_episodes
+
+    end_time = time.time()
+    print ("Estimate of Pr(S_8=19 | S_19 = 22) = ", Pr_s19_22_given_s8_19)
+    print ("Execution time = ", end_time - start_time)
+    """
+        Estimate of Pr(S_8=19 | S_19 = 22) =  0.01873
+    """
+
+def quantile(discounted_returns):
+
+    num_returns=len(discounted_returns)
+    discounted_returns.sort()
+
+    cdf = []
+    print("CDF")
+
+    for  r in sorted(set(discounted_returns)):
+        cdf.append (len([i for i in discounted_returns if i<= r])/num_returns)    
+
+    return cdf, sorted(set(discounted_returns))
+
 def main():
-    print("Hello world")
+    np.random.seed(123)
+
+    returns_A = problemA()
+    x,y = quantile(returns_A)
+    plt.title("Quantile Function for Random Policy")
+    plt.xlabel("Sample Fraction (CDF)")
+    plt.ylabel("Quantile (Discounted Return)")
+    plt.plot(x,y)
+    plt.show()
+    
+    returns_B = problemB()
+    x,y = quantile(returns_B)
+    plt.plot(x,y)
+    plt.title("Quantile Function for Optimal Policy")
+    plt.xlabel("Sample Fraction (CDF)")
+    plt.ylabel("Quantiles (Discounted Return)")
+    plt.show()
+    quantile(returns_B)
+
+    # problemE()
 
 main()
         
--- rl687/environments/gridworld.py
+++ rl687/environments/gridworld.py
@@ -5,7 +5,7 @@ class Gridworld(Environment):
     """
     The Gridworld as described in the lecture notes of the 687 course material. 
     
-    Actions: up (0), down (1), left (2), right (3)
+    Actions: up (0), right (1), down (2), left (3),
     
     Environment Dynamics: With probability 0.8 the robot moves in the specified
         direction. With probability 0.05 it gets confused and veers to the
@@ -29,37 +29,88 @@ class Gridworld(Environment):
     """
 
     def __init__(self, startState=0, endState=24, shape=(5,5), obstacles=[12, 17], waterStates=[6, 18, 22]):
-        pass
+        self.startState = startState
+        self.endState = endState
+        self.shape = shape
+        self.obstacles = obstacles
+        self.waterStates = waterStates
+
+        self._state = startState
+        self._action = -1    # no action taken yet
+        self._reward = 0     # no reward yet
+        self._gamma = 0.9
+        self.gamma_coeff = 1
         
     @property
     def name(self):
         pass
-        
+
     @property
     def reward(self):
-        pass
+        return self._reward
 
     @property
     def action(self):
-        pass
+        return self._action
 
     @property
     def isEnd(self):
-        pass
+        return self._state == self.endState
 
     @property
     def state(self):
-        pass
+        return self._state
 
     @property
     def gamma(self):
-        pass
+        return self._gamma
 
     def step(self, action):
-        pass
+        p = np.random.random_sample()
+        # veer right with prob 0.05
+        actual_action = action
+        if p < 0.05:
+            actual_action = (action+1)%4
+        # veer left with prob 0.05
+        elif p < 0.1:
+            actual_action = (action-1)%4
+        # stay there with prob 0.1
+        elif p < 0.2:
+            actual_action = -1
+
+        # move up if possible
+        if (actual_action == 0):
+            new_state = self._state - self.shape[1]
+            if (new_state >= 0 and new_state not in self.obstacles):
+                self._state = new_state
+        # move right if possible
+        elif (actual_action == 1):
+            new_state = self._state + 1
+            if (self._state%5 != 4 and new_state not in self.obstacles):
+                self._state = new_state
+        # move down if possible
+        elif (actual_action == 2):
+            new_state = self._state + self.shape[1]
+            if (new_state < 25 and new_state not in self.obstacles):
+                self._state = new_state
+        # move left if possible
+        elif (actual_action == 3):
+            new_state = self._state - 1
+            if (self._state%5 != 0 and new_state not in self.obstacles):
+                self._state = new_state
+
+        self._action = action           # Update A_t
+        self._reward = self.gamma_coeff * self.R(self._state)  # Update R_t
+        self.gamma_coeff *= self._gamma
+        return (actual_action, self._state, self._reward)
+
 
     def reset(self):
-        pass
+        self._state = self.startState
+        self._action = -1    # no action taken yet
+        self._reward = 0     # no reward yet
+        self._gamma = 0.9
+        self.gamma_coeff = 1
         
     def R(self, _state):
         """
@@ -68,4 +119,10 @@ class Gridworld(Environment):
         output:
             reward -- the reward resulting in the agent being in a particular state
         """
-        pass
+        if _state == self.endState:
+            self._reward = 10
+        elif _state in self.waterStates:
+            self._reward = -10
+        else:
+            self._reward = 0
+        return self._reward
